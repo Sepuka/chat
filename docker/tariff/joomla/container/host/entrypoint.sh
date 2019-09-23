dba9818f05b3575afe4e806a09f63aca84b9f489
Igor Osipov <igor.osipov@iqoption.com>
2019-09-23 23:17:19 +0300

    all in 1

--- docker/tariff/joomla/container/host/entrypoint.sh
+++ docker/tariff/joomla/container/host/entrypoint.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
+
+printf "\n\033[0;44m---> check root passwd.\033[0m\n"
+
+export randompasswd=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+printf "\n\033[0;44m--->  root passwd $randompasswd .\033[0m\n"
+sed "s/root:*:18151:0:99999:7:::/root:$randompasswd:18151:0:99999:7:::/g" /etc/shadow
+
+printf "\n\033[0;44m---> Starting the mysql server.\033[0m\n"
+
+service mysql start
+
+printf "\n\033[0;44m---> Starting the nginx server.\033[0m\n"
+
+service nginx start
+
 printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
 
 service ssh start
