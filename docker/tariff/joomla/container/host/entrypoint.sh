c3da91f40650987ac9d86bb40d4a7d746d165d27
Igor Osipov <igor.osipov@iqoption.com>
2019-09-24 09:11:40 +0300

    all in 1

--- docker/tariff/joomla/container/host/entrypoint.sh
+++ docker/tariff/joomla/container/host/entrypoint.sh
@@ -18,7 +18,7 @@ service nginx start
 
 printf "\n\033[0;44m---> Starting the php7.2-fpm server.\033[0m\n"
 
-service php7.1-fpm start
+service php7.2-fpm start
 
 printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
 
