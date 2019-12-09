e81ed74032ceaf609133a2c2a59ad05adb1c2a09
Maksim Shlomin <m.shlomin@corp.vk.com>
2019-12-09 17:22:56 +0300

    ssh virtual host pass

--- docker/tariff/joomla/container/host/entrypoint.sh
+++ docker/tariff/joomla/container/host/entrypoint.sh
@@ -6,7 +6,7 @@ printf "\n\033[0;44m---> check root passwd.\033[0m\n"
 
 export randompasswd=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 printf "\n\033[0;44m--->  root passwd $randompasswd .\033[0m\n"
-echo "root:$randompasswd" | chpasswd
+echo "root:secret" | chpasswd
 printf "\n\033[0;44m---> Starting the mysql server.\033[0m\n"
 
 service mysql start
